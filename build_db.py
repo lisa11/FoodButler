65999ae56ed087f516335545875f6aa9b1622063
Xinyi Ge <xinyige@cs.uchicago.edu>
2016-02-19 21:39:40 -0600

    code done; need to try

--- build_db.py
+++ build_db.py
@@ -1,5 +1,5 @@
 from yummly import Client
-import csv
+import json
 # default option values
 TIMEOUT = 5.0
 RETRIES = 0
@@ -9,7 +9,89 @@ YOUR_API_KEY = "eb5bef37e3d25571eafd42e98ace7ee7"
 
 client = Client(api_id=YOUR_API_ID, api_key=YOUR_API_KEY, timeout=TIMEOUT, retries=RETRIES)
 #search = Client.search("green eggs and ham")
-params = ["beef", "pork", "chicken", "turkey", "fish"]
+
+def find_recipes(client, param, recipe_db, course, max_time):
+    recipe_list = []
+    param["requirePictures"] = True
+    param["allowedCourse[]"] = [course]
+    param["maxTotalTimeInSeconds"] = max_time
+    recipes = client.search(param)
+    for match in recipes.matches:
+        ingredients = match["ingredients"]
+        recipe_id = match["id"]
+        if recipe_id in recipe_db:
+            recipe_list.append((recipe_db[recipe_id], ingredients))
+        else:
+            recipe = client.recipe(recipe_id)
+            recipe_list.append((recipe, ingredients))
+            recipe_db[recipe_id] = recipe
+
+def go(param):
+    with open("recipe_db.json") as f:
+        recipe_db = json.load(f)
+
+    breakfast_maxtime, maindish_maxtime = param["maxTotalTimeInSeconds"]
+
+    breakfast_list = find_recipes(client, param, recipe_db, "Breakfast and Brunch", breakfast_maxtime)  
+    main_dish_list = find_recipes(client, param, recipe_db, "Main Dishes", maindish_maxtime)
+
+    if "allowedIngredient[]" in param:
+        if "excludedIngredient[]" in param:
+            param["excludedIngredient[]"].extend(param["allowedIngredient"])
+        else:
+            param["excludedIngredient"] = param["allowedIngredient"]
+        del param["allowedIngredient"]
+        breakfast_alt_list = find_recipes(client, param, recipe_db, "Breakfast and Brunch", breakfast_maxtime)  
+        main_dish_alt_list = find_recipes(client, param, recipe_db, "Main Dishes", maindish_maxtime)
+    else: 
+        breakfast_alt_list = []
+        main_dish_alt_list = []
+
+    with open("recipe_db.json", "w") as f:  
+        f.write(json.dumps(recipe_db))
+
+    return(breakfast_alt_list, breakfast_list, main_dish_alt_list, main_dish_list)
+
+
+
+if __name__ == "__main__":
+    go(sys.argv)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 with open("recipe_csv.csv", "w") as f:   # may change the file name to "meat_dish_csv.csv"
     for param in params:
         results = client.search(param)
