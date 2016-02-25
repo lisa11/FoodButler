f33140ee0eb425953926765510a492138f6be53b
Guchuan Song <gsong@cs.uchicago.edu>
2016-02-25 17:19:49 -0600

    shopping list

--- shopping_list.py
+++ shopping_list.py
@@ -1,32 +1,37 @@
 # Must be used together with input from generate_menu.py or front end
 
 def generate_shopping_list(result):
-	'''
+    '''
 	generate the shopping list after finalizing menu
 	result: final_output from backend after finalizing for alternative changes
     output: a list with each element a list of 2 elements -- the 
     ingredient needed and a list of days on which this ingredient is needed
     '''
 
-    breakfast_final_list, lunch_list, dinner_list, calories_list, alternative_breakfast_list, alternative_lunch_list, alternative_dinner_list = result
     rv_dict = {}
 
     for i in range(7):
-        if breakfast_final_list[i]["ingredients"] not in rv_dict:
-    	    rv_dict[breakfast_final_list[i]["ingredients"]] = [i + 1]
-    	else:
-    		rv_dict[breakfast_final_list[i]["ingredients"]].append(i + 1)
-    	if lunch_list[i]["ingredients"] not in rv_dict:
-    	    rv_dict[lunch_list[i]["ingredients"]] = [i + 1]
-    	else:
-    		rv_dict[lunch_list[i]["ingredients"]].append(i + 1)
-    	if dinner_list[i]["ingredients"] not in rv_dict:
-    	    rv_dict[dinner_list[i]["ingredients"]] = [i + 1]
-    	else:
-    		rv_dict[dinner_list[i]["ingredients"]].append(i + 1)
+        for j in range(len(result[0][i]["ingredients"])):
+            if result[0][i]["ingredients"][j] not in rv_dict:
+    	        rv_dict[result[0][i]["ingredients"][j]] = [i + 1]
+            else:
+                rv_dict[result[0][i]["ingredients"][j]].append(i + 1)
+        for j in range(len(result[1][i]["ingredients"])):
+            if result[1][i]["ingredients"][j] not in rv_dict:
+                rv_dict[result[1][i]["ingredients"][j]] = [i + 1]
+            else:
+                rv_dict[result[1][i]["ingredients"][j]].append(i + 1)
+        for j in range(len(result[2][i]["ingredients"])):
+            if result[2][i]["ingredients"][j] not in rv_dict:
+                rv_dict[result[2][i]["ingredients"][j]] = [i + 1]
+            else:
+                rv_dict[result[2][i]["ingredients"][j]].append(i + 1)
     
     rv_list = []
     for x in rv_dict:
     	rv_list.append([x, rv_dict[x]])
+
+    with open("shopping_list_sample.txt", "w") as f:
+        print(rv_list, file = f)
 
     return rv_list
