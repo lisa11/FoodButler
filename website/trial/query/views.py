5b7e836dd9d17b4ef538d2a6c85bb01331597b53
Lisa Li <lisali11@uchicago.edu>
2016-02-28 12:31:46 -0600

    saving

--- website/trial/query/views.py
+++ website/trial/query/views.py
@@ -51,18 +51,21 @@ DIET = ['Lacto vegetarian', 'Ovo vegetarian', 'Pescetarian', 'Vegan', 'Vegetaria
 SERVINGS = [1, 2, 3, 4]
 
 class SearchForm(forms.Form):
-    avoid=forms.CharField(
-        label="Ingredients to avoid", 
-        #help_text="ingredients you want to avoid",
-        #required=False)
-        )
-    already_have=forms.CharField(
-        label='Ingredients already have', 
-        #help_text="ingredients you already have",
+    ingredients_avoid=forms.CharField(
+        label="Avoiding Ingredients", 
+        #help_text="(Ingredients to avoid)",
+        required=False)
+    ingredients_already_have=forms.CharField(
+        label='Including Ingredients', 
+        #help_text="(Ingredients to include in meal)",
+        required=False)
+    number_of_meal=forms.IntegerField(
+        label='Number of Meals',
+        help_text='(Number of meals to have Including Ingredients)', 
+        min_value=1,
+        max_value=21,
         required=False)
     allergy=forms.MultipleChoiceField(
-        #fields=(forms.IntegerField(),
-        #          forms.IntegerField()),
         label='Allergy',
         choices=[(x, x) for x in ALLERGIES],
         widget=forms.CheckboxSelectMultiple, 
@@ -72,6 +75,34 @@ class SearchForm(forms.Form):
         choices= [(x, x) for x in DIET],
         widget=forms.CheckboxSelectMultiple, 
         required=False)
+    time_breakfast = forms.IntegerField(
+        label="Maximum Breakfast Cooking Time",
+        #help_text="(Maximun breakfast cooking time)",
+        min_value=0,
+        max_value=300,
+        required=False)
+    time_meal = forms.IntegerField(
+        label="Maximum Lunch/Dinner Cooking Time",
+        #help_text="(Maximun lunch or dinner cooking time)",
+        min_value=0,
+        max_value=300, 
+        required=False)
+    servings=forms.ChoiceField(
+        label="Serving Size", 
+        choices=[(x, x) for x in range(1, 5)],
+        #required=False)
+        )
+    calories_lower=forms.IntegerField(
+        label="Minimum Calories Per Day", 
+        min_value=0,
+        max_value=5000,
+        required=False)
+    calories_upper=forms.IntegerField(
+        label="Maximum Calories Per Day",
+        min_value=0,
+        max_value=5000, 
+        required=False)
+
     ###cooking_time=IntegerRange(widget=RANGE_WIDGET)#ValueField(
         #required=False)
     ###calories=IntegerRange(widget=RANGE_WIDGET)
@@ -98,8 +129,34 @@ def query(request):
         form = SearchForm(request.GET)
 
         if form.is_valid():
+            args={}
+            if form.cleaned_data['ingredients_already_have']:
+                args['allowedIngredient[]'] = form.cleaned_data['ingredients_already_have'].split()
+            
+            if form.cleaned_data['ingredients_avoid']:
+                args['excludedIngredient[]'] = form.cleaned_data['ingredients_avoid'].split()
+            
+            if form.cleaned_data['number_of_meal']:
+                args['meal_number'] = form.cleaned_data['number_of_meal']
+
+            if form.cleaned_data['allergy']:
+                args['allowedAllergy[]'] = form.cleaned_data['allergy']
+            
+            if form.cleaned_data['diet']:
+                args['allowedDiet[]'] = form.cleaned_data['diet']
+            
+            if form.cleaned_data['time_breakfast'] and form.cleaned_data['time_meal']:
+                args['maxTotalTimeInSeconds'] = [form.cleaned_data['time_breakfast'], form.cleaned_data['time_meal']]
+            
+            if form.cleaned_data['servings']:
+                args['servings'] = form.cleaned_data['servings']
+            
+            if form.cleaned_data['calories_lower'] and form.cleaned_data['calories_upper']:
+                args['calories_per_day'] = [form.cleaned_data['calories_lower'], form.cleaned_data['calories_upper']]
+            
             #logging.error("what is this" % (form))
-            return custom_redirect('results', **form.cleaned_data)
+            #return custom_redirect('results', **form.cleaned_data)
+            return render(request, "query/results.html", {"c":args})
     else:  
         form = SearchForm()
         #else:
