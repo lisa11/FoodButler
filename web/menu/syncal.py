b10b65ce1ccf326eeaef23224fdf45174b8f99f8
Jiacheng Yan <yanjc@cs.uchicago.edu>
2016-03-06 13:25:51 -0600

    syncal

--- web/menu/syncal.py
+++ web/menu/syncal.py
@@ -3,50 +3,53 @@
 # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
 # stored credentials.
 
+import datetime
 
-def syn_one_meal(one_meal_list, start_time, end_time, start_date, reminders = False):
-    event = {}
-    
+
+
+def build_event_l(one_meal_list, start_time_in, start_date):
+    '''
+    example start_date: [2016,3,18]
+    example start_time: [8,0,0]   i.e. 8:00:00
+    '''
+    year, month, day = start_date
+    hour, minute, secend = start_time_in
+    start_datetime = datetime.datetime(year, month, day, hour, minute, second)
+    end_datetime = start_datetime + datetime.timedelta(hours = 2)
+    rv_event_l = []
     for meal in one_meal_list:
+        event = {}    
         event["summary"] = meal["name"]
         event["description"] = "calories: " + meal["calories"] + "    cooking time: " + meal["cooking_time"]
+        start_datetime_s = str(start_datetime)
+        s_date, s_time = start_datetime_s.split()
+        start_time = s_date + "T" + s_time + "-06:00"
+        end_datetime = start_datetime + datetime.timedelta(hours = 2)
+        end_time_s = str(end_datetime)
+        e_date, e_time = end_time_s.split()
+        end_time = e_date +  "T" + e_time + "-06:00"
         event["start"] = {"dateTime": start_time, "timeZone": "America/Chicago"}
         event["end"] = {"dateTime": end_time, "timeZone": "America/Chicago"}
-        event['reminders'] = {'useDefault': reminders, 'overrides': [{'method': 'email', 'minutes': 24 * 60},
-            {'method': 'popup', 'minutes': 10},]}
-    return event
+        start_datetime = start_datetime + datetime.timedelta(days = 1)
+        rv_event_l.append(event)
+    return rv_event_l
 
 
 def syn_to_calendar(start_date):
-
+    '''
+    start_date example: [2016,3,10]
+    '''
     with open("final_output.txt") as f:
         breakfast_list, lunch_list, dinner_list, calories_list, alternative_breakfast_list, alternative_lunch_list, alternative_dinner_list = f.readline()
-    bre
+    breakfast_events = build_event_l(breakfast_list, [8,0,0], start_date)
+    lunch_events = build_event_l(lunch_list, [11,30,0], start_date)
+    dinner_events = build_event_l(dinner_list, [5,0,0], start_date)
 
-event = {
-  'summary': 'Google I/O 2015',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2015-05-28T09:00:00-06:00',
-    'timeZone': 'America/Chicago',
-  },
-  'end': {
-    'dateTime': '2015-05-28T17:00:00-06:00',
-    'timeZone': 'America/Chicago',
-  },
-  'reminders': {
-    'useDefault': False,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10},
-    ],
-  },
-}
+    for event in breakfast_events + lunch_events + dinner_events:
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print 'Event created: %s' % (event.get('htmlLink'))
 
-event = service.events().insert(calendarId='primary', body=event).execute()
-print 'Event created: %s' % (event.get('htmlLink'))
-
-
+'''
 def add_attachment(calendarService, driveService, calendarId, eventId, fileId):
     file = driveService.files().get(fileId=fileId).execute()
     event = calendarService.events().get(calendarId=calendarId,
@@ -65,3 +68,13 @@ def add_attachment(calendarService, driveService, calendarId, eventId, fileId):
     calendarService.events().patch(calendarId=calendarId, eventId=eventId,
                                    body=changes,
                                    supportsAttachments=True).execute()
+'''
+
+
+
+
+
+
+
+
+
